6c2eb47cc6e8cec9e26ec737e5b15697990d356d
PapuMcLovin <jesus.garciardrg@uanl.edu.mx>
2024-01-17 15:16:03 -0600

    chasis 0.1

--- MacCommand/src/main/java/frc/robot/Comandos/ChasisCmd.java
+++ MacCommand/src/main/java/frc/robot/Comandos/ChasisCmd.java
@@ -1,9 +1,39 @@
 package frc.robot.Comandos;
 
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Subsistemas.ChasisSub;
 
 public class ChasisCmd extends Command{
     
+    private final ChasisSub chasisSub;
+    private final Supplier<Double> VelFun, GiroFun;
+
+    public ChasisCmd (ChasisSub chasisSub, Supplier<Double> VelFun, Supplier<Double> GiroFun) {
+        this.VelFun = VelFun;
+        this.GiroFun = GiroFun;
+        this.chasisSub = chasisSub;
+        addRequirements(chasisSub);
+
+    }
+
+  @Override
+  public void initialize () {
+    ChasisSub.MotorAgrup();
+    
+  }
+
+  @Override
+  public void execute () {}
+
+  @Override
+  public void end (boolean interrupted) {}
+
+  @Override
+  public boolean isFinished () {
+    return false;
+  }
 }
--- MacCommand/src/main/java/frc/robot/RobotContainer.java
+++ MacCommand/src/main/java/frc/robot/RobotContainer.java
@@ -4,19 +4,23 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Comandos.ChasisCmd;
+import frc.robot.Subsistemas.ChasisSub;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
 
-  private void configureBindings() {}
+  public final static ChasisSub chasisSub = new ChasisSub();
+  public Joystick Cntrl = new Joystick(0);
+
+  public RobotContainer() {
+    chasisSub.setDefaultCommand(new ChasisCmd(chasisSub, () -> (Cntrl.getRawAxis(5) ), () -> -Cntrl.getRawAxis(0)));
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
-
-//calando si jala chido xd
 }
